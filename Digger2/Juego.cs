b182a8b09e7d111ddcb41d496eae05f4190f935e
Max Sebastián Saldaña <vloxomire@gmail.com>
2020-08-27 21:01:46 -0300

    Main->Juego
    Pasando caaampos, y metodos

--- Digger2/Juego.cs
+++ Digger2/Juego.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,50 @@ namespace Digger2
 {
     class Juego
     {
+        RenderWindow ventana;
+        public Juego()
+        {
+            //ventana
+            ventana = new RenderWindow(new VideoMode(320, 480), "Digger2");
+        }
         public void Comienzo() 
         {
-
+            //ventana
+            ventana.SetFramerateLimit(60);
+            //Escuchadores
+            ventana.Closed += CerrarVentana;//escuchador de cerrar ventana con x.
+            ventana.KeyReleased += SoltarEsc;//escuchador de cerrar cuando suelto una tecla.
+            ventana.KeyPressed += PresionarShift;//escuchador para cerrar presionando una tecla
+            //Inicio del Juego, bucle
+            while (ventana.IsOpen)
+            {
+                ventana.DispatchEvents();//escuchadores
+                ventana.Draw(rigby);
+                ventana.Display();//Muestro la ventana
+            }
+        }
+        //Cierro con x
+        private void CerrarVentana(object sender, EventArgs e)
+        {
+            ventana.Close();
+        }
+        //Cierro soltando
+        private void SoltarEsc(object sender, KeyEventArgs e)
+        {
+            var win = (Window)sender;
+            if (e.Code == Keyboard.Key.Escape)
+            {
+                win.Close();
+            }
+        }
+        //Cierro presionando
+        private void PresionarShift(object sender, KeyEventArgs e)
+        {
+            var win = (Window)sender;
+            if (e.Code == Keyboard.Key.LShift)
+            {
+                win.Close();
+            }
         }
     }
 }
--- Digger2/MainP.cs
+++ Digger2/MainP.cs
@@ -7,48 +7,14 @@ namespace Digger2
 {
     class MainP
     {
-        //ventana
-        static RenderWindow renderWindow = new RenderWindow(new VideoMode(320, 480), "Digger2");
         static void Main(string[] args) 
         {
-            //Escuchadores
-            renderWindow.Closed +=CerrarVentana;//escuchador de cerrar ventana con x.
-            renderWindow.KeyReleased +=SoltarEsc;//escuchador de cerrar cuando suelto una tecla.
-            renderWindow.KeyPressed += PresionarShift;//escuchador para cerrar presionando una tecla
+            //Campos
+            Juego juego = new Juego();
+            juego.Comienzo();
             //player
             var rigby = new Sprite(new Texture("sprite/minero.png"));
             rigby.Position = new Vector2f(0.0f,0.0f);
-            
-            //Inicio del Juego, bucle
-            while (renderWindow.IsOpen) 
-            {
-                renderWindow.DispatchEvents();//escuchadores
-                renderWindow.Draw(rigby);
-                renderWindow.Display();//Muestro la ventana
-            }
-        }
-        //Cierro con x
-        private static void CerrarVentana(object sender,EventArgs e) 
-        {
-            renderWindow.Close();
-        }
-        //Cierro soltando
-        private static void SoltarEsc(object sender,KeyEventArgs e) 
-        {
-            var win = (Window)sender;
-            if (e.Code==Keyboard.Key.Escape)
-            {
-                win.Close();
-            }
-        }
-        //Cierro presionando
-        private static void PresionarShift(object sender,KeyEventArgs e) 
-        {
-            var win = (Window)sender;
-            if (e.Code==Keyboard.Key.LShift)
-            {
-                win.Close();
-            }
         }
     }
 }
